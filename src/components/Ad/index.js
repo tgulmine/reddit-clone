97d2076a388f15287faf11b56b088332b8f2930c
tgulmine <tgulmine@gmail.com>
2019-11-28 01:57:48 -0300

    ad night mode

--- src/components/Ad/index.js
+++ src/components/Ad/index.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./Ad.scss";
 
-export default class Ad extends React.Component {
-  getImage(imageUrl) {
-    return { backgroundImage: `url("${imageUrl}")` };
-  }
+function Ad(props) {
+  const nightMode = props.nightMode;
 
-  render() {
-    return (
-      <div className="ad-container">
-        <div className="ad-title">ADVERTISEMENT</div>
-        <div
-          className="ad-image"
-          style={this.getImage(
-            "https://upload.wikimedia.org/wikipedia/commons/a/a7/Target_dog.jpg"
-          )}
-        />
+  return (
+    <div
+      className={
+        !nightMode ? "ad-container" : "ad-container ad-container--night"
+      }
+    >
+      <div className={!nightMode ? "ad-title" : "ad-title ad-title--night"}>
+        ADVERTISEMENT
       </div>
-    );
-  }
+      <div
+        className="ad-image"
+        style={{
+          backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/a/a7/Target_dog.jpg)`
+        }}
+      />
+    </div>
+  );
 }
+
+export default Ad;
