a37d974a8bc2444692a9824c8027e354c58e0958
tgulmine <tgulmine@gmail.com>
2019-12-03 23:10:00 -0300

    add subreddit routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./pages/App/App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <Route path="/">
+      <Route
+        path="/r/announcements"
+        render={props => <App {...props} idSub={1} />}
+      />
+      <Route path="/r/funny" render={props => <App {...props} idSub={2} />} />
+      <Route
+        path="/r/AskReddit"
+        render={props => <App {...props} idSub={3} />}
+      />
+      <Route path="/r/gaming" render={props => <App {...props} idSub={4} />} />
+      <Route path="/r/pics" render={props => <App {...props} idSub={5} />} />
+      <Route path="/r/science" render={props => <App {...props} idSub={6} />} />
+      <Route
+        path="/r/worldnews"
+        render={props => <App {...props} idSub={7} />}
+      />
+      <Route path="/r/aww" render={props => <App {...props} idSub={8} />} />
+      <Route path="/r/movies" render={props => <App {...props} idSub={9} />} />
+      <Route
+        path="/r/todayilearned"
+        render={props => <App {...props} idSub={10} />}
+      />
+      {/* <Route path="*" render={props => <App {...props} idSub={3} />} /> */}
+    </Route>
+  </Router>,
+  document.getElementById("root")
+);
+
+/* ReactDOM.render(<App />, document.getElementById("root")); */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import axios from "axios";
 import "../../styles/main.scss";
 /* import PostList from "./posts/PostList"; */
 import Header from "../../components/Header/index";
@@ -6,9 +8,24 @@ import Banner from "../../components/Banner/index";
 import SortBar from "../../components/SortBar/index";
 import PostArea from "../../components/PostArea/index";
 
-function App() {
-  const idSub = 7;
+function App(props) {
+  const [sub, setSub] = useState({});
+  const idSub = props.idSub;
   const [nightMode, setNightMode] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://my-json-server.typicode.com/tgulmine/reddit-clone/subs/${idSub}`
+      )
+      .then(res => {
+        console.log(res);
+        setSub(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [idSub]);
 
   function triggerNightMode() {
     setNightMode(!nightMode);
@@ -16,16 +33,19 @@ function App() {
   }
 
   return (
-    <div className="app-container">
-      <Header
-        idSub={idSub}
-        nightMode={nightMode}
-        triggerNightMode={triggerNightMode}
-      />
-      <Banner idSub={idSub} nightMode={nightMode} />
-      <SortBar idSub={idSub} nightMode={nightMode} />
-      <PostArea idSub={idSub} nightMode={nightMode} />
-    </div>
+    <>
+      <Helmet title={sub.page_name} />
+      <div className="app-container">
+        <Header
+          idSub={idSub}
+          nightMode={nightMode}
+          triggerNightMode={triggerNightMode}
+        />
+        <Banner idSub={idSub} nightMode={nightMode} />
+        <SortBar idSub={idSub} nightMode={nightMode} />
+        <PostArea idSub={idSub} nightMode={nightMode} />
+      </div>
+    </>
   );
 }
 
