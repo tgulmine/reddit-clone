8b9aca79fd0e9d4d1ee138c3d71b623e02cebb64
tgulmine <tgulmine@gmail.com>
2020-07-02 22:29:43 -0300

    fix warnings

--- src/components/PostArea/index.js
+++ src/components/PostArea/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./PostArea.scss";
-import CreatePost from "../CreatePost/index";
 import Post from "../Post/index";
 import About from "../About/index";
 import Ad from "../Ad/index";
